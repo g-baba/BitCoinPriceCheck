6d621a3b4d5f379140d3e8dbb0ee3c0315f7708d
g-baba <38183765+g-baba@users.noreply.github.com>
2019-12-12 14:59:02 -0800

    Update bitcoin.py

--- src/bitcoin.py
+++ src/bitcoin.py
@@ -3,22 +3,11 @@ import json
 
 coins = [
             [1,  'BTC',  'btc_jpy'],
-            [2,  'ETH',  'eth_jpy'],
-            [3,  'ETC',  'etc_jpy'],
-#            [4,  'DAO',  'dao_jpy'], Not available
-            [5,  'LSK',  'lsk_jpy'],
-            [6,  'FCT',  'fct_jpy'],
-#            [7,  'XMR',  'xmr_jpy'], Not available
-#            [8,  'REP',  'rep_jpy'], Not available
-            [9,  'XRP',  'xrp_jpy'],
-#            [10, 'ZEC',  'zec_jpy'], Not available
-            [11, 'XEM',  'xem_jpy'],
-            [12, 'LTC',  'ltc_jpy'],
-#            [13, 'DASH', 'dash_jpy'], Not available
-            [14, 'BCH',  'bch_jpy'],
+            [2,  'XEM',  'xem_jpy'],
+            [3,  'MONA', 'mona_jpy'],
         ]
 
-urlbase = 'https://coincheck.com/api/rate/'
+urlbase = 'https://api.zaif.jp/api/1/last_price/'
 
 def main():
     for i in range(len(coins)):
@@ -28,7 +17,7 @@ def main():
 
         rate = json.loads(response.text)
 
-        print("%-4s : \%-10s" % (coins[i][1], rate['rate']))
+        print("%-4s : \%-10s" % (coins[i][1], rate['last_price']))
 
 if __name__ == "__main__":
     main()
